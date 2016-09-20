df3b2f7eb13b67060813928c62cedef95190240f
NickGarcia <nick.agarcia103@gmail.com>
2016-09-20 11:05:26 -0500

    change classie script in footer

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
         window.addEventListener('scroll', function(e) {
             var distanceY = window.pageYOffset || document.documentElement.scrollTop,
                 shrinkOn = 300,
-                header = document.querySelector("nav");
+                header = document.querySelector("header");
             if (distanceY > shrinkOn) {
                 classie.add(header, "smaller");
             } else {
